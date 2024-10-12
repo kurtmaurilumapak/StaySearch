313c92c31c2cad7cc1eef63909d6616f360cf4b9
Your Name <you@example.com>
2024-10-12 13:49:42 +0800

    added partial landing

--- src/router/index.js
+++ src/router/index.js
@@ -5,8 +5,8 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'home',
-      //component: HomeView
+      name: 'landing',
+      component: () => import('@/views/system/LandingView.vue')
     },
     {
       path: '/signup',
