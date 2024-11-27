aa8f3270c3facff1a0d45ba9f9f291fdc6cd0f24
elyharoldflorendo <elyharoldflorendo72@gmail.com>
2024-11-28 00:06:55 +0800

    fix:hopefully functional reject and accept

--- src/views/system/admin/Approved.vue
+++ src/views/system/admin/Approved.vue
@@ -1,6 +1,14 @@
 <script setup>
 import AdminNavbar from '@/components/common/AdminNavbar.vue'
 import Applayout from '@/components/layout/AppLayout.vue'
+import { useAdminStore } from '@/stores/adminStore';
+import { onMounted } from 'vue';
+
+const adminStore = useAdminStore();
+onMounted(() =>{
+  adminStore.fetchCards();
+})
+
 </script>
 
 <template>
@@ -9,8 +17,12 @@ import Applayout from '@/components/layout/AppLayout.vue'
       <AdminNavbar />
       <v-row dense>
         <v-col col="12" md="6">
-          <v-card>
-            <v-card-title> Boarding House Name </v-card-title>
+          <v-card v-for="card in adminStore.cards":key="boardinghouse.id">
+            <v-card-title>{{card.name}}</v-card-title>
+            <v-card-text>{{card.address}}</v-card-text>
+            <v-card-text>{{card.price}}</v-card-text>
+            <v-card-text>{{card.decription}}</v-card-text>
+            <v-card-text>{{card.status}}</v-card-text>
           </v-card>
         </v-col>
       </v-row>
--- src/views/system/admin/Request.vue
+++ src/views/system/admin/Request.vue
@@ -26,12 +26,12 @@ const handleReject = async (cardID) => {
     <template #content>
       <AdminNavbar />
       <v-row justify="space-around">
-        <v-col cols="12" md="6" v-for="card in adminStore.cards" :key="card_id">
+        <v-col cols="12" md="6" v-for="card in adminStore.cards":key="card.id">
           <v-card class="mx-auto align-start">
             <v-card-title>{{card.name}}</v-card-title>
             <v-card-text>{{card.address}}</v-card-text>
-            <v-card-text>{{card.decreiption}}</v-card-text>  
             <v-card-text>{{card.price}}</v-card-text>
+            <v-card-text>{{card.decription}}</v-card-text>
             <v-card-actions justify="end">
               <v-btn color="green" text @click="handleApprove">Approve</v-btn>
               <v-btn color="red" text @click="handleReject">Reject</v-btn>
